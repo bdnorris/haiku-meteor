92373f5de322bb57315ebfad67b02b21679ea603
Brian Norris <bnorris@marlinco.com>
2016-03-10 20:30:52 -0600

    Cleanup

--- haiku.js
+++ haiku.js
@@ -1,83 +1,18 @@
 HaikuLines = new Mongo.Collection("lines");
 
 if (Meteor.isClient) {
-    // This code only runs on the client
     
-    //var a = HaikuLines.find({}, {}, {});
-    //var a = HaikuLines.findOne({}, {rnd:{ $gte: Math.random() }}, {$orderby: { rnd: 1 }});
     var a = HaikuLines.find({rnd:{$gte: Math.random()}}, {limit: 3}, {});
-    
-    //console.log(a.collection.queries[1].results[0]);
     
     Template.body.helpers({
         lines: function() {
             return HaikuLines.find({}, {sort: {createdAt: -1}});
         },
         randLines: function() {
-            //return HaikuLines.findOne({}, {rnd:{ $gte: Math.random() }}, {$orderby: { rnd: 1 }});
-            
-            
-            //var query = { state: 'OK' };
-            //n = HaikuLines.count({});
-            //console.log(n);
-            //var r = Math.floor(Math.random() * n);
-            //return HaikuLines.find(query).limit(1).skip(r);
-            //function getThreeLines() {
-               
-                    //var a = HaikuLines.find({}, {}, {});
-                    //console.log(a.collection.queries[1].results[0]);
-                    return a;
-            
-            
-            
-              //  for each (lines in a) {
-                //    console.log(line);
-                //}
-            
-            
-                
-            //}
-            
-            //console.debug(c);
-            
-            //var r = Math.floor(Math.random());
-            //var r = Math.floor(Math.random() * 0.99999999999999) + 0; 
-            //var r = Math.random() * 0.99999999999999 + 0;
-            //var r = Math.random() * 1 + 0;
-            //console.debug(r);
-            //return HaikuLines.find({}, {limit: 3}, {skip: r});
-            
-            //return HaikuLines.find({rnd: {$gt:r}}, {limit: 3}, {skip: r});
-            //return db.HaikuLines.find({rand:{$gt:r}}).sort({rand:1}).limit(3);
-            //return HaikuLines.find().skip(c).limit(1);
-            //limit(3).skip(r);
+            return a;
         },
-        
-       /* randLines: function() {
-            var myArr = HaikuLines.find({}, {text:1, _id:0});   
-            var arr = Object.values(myArr);
-            console.log(arr);
-            return myArr;
-            
-        }*/
-        
-
-    
-        
-        // Lets instead, make two arrays, one for all of the 5 syl and one for all of the 2 syl on load.
-        
-        
-        
     });
-    
 
-    
-    
-    
-    
-    
-    // Event Handler
-    // Template . templateName . events 
     Template.body.events({
         "submit .new-line": function(event, syl) {
             // Prevent default browser form submit
@@ -88,8 +23,6 @@ if (Meteor.isClient) {
             // text.value gets the value
             var text = event.target.text.value;
             var syl = event.target.syl.value;
-            //var syl = syl.target.text.value;
-            //console.log(event);
             
             // Insert a task into the collection 
             // Tasks is the Mongo.Collection("tasks")
@@ -106,18 +39,12 @@ if (Meteor.isClient) {
         }
     });
     
-     Template.line.events({
-    //"click .toggle-checked": function () {
-        // Set the checked property to the opposite of its current value
-        //HaikuLines.update(this._id, {
-        //    $set: {checked: ! this.checked}
-        //  });
-   // },
-    "click .delete": function () {
-      HaikuLines.remove(this._id);
-    },
-  });
-    
+    Template.line.events({
+        "click .delete": function () {
+          HaikuLines.remove(this._id);
+        },
+    });
+
     Template.randLine.events ({
         
     });
