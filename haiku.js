c5b430ce9ea602ca81933312226dd2971d1f5e6c
Brian Norris <bnorris@marlinco.com>
2016-06-21 13:17:52 -0500

    Good stopping point.
    Added a defaultHelper template helper to show the route name as an id of a container. Reconfigured sass file to use these new ids and display the homepage differently than the admin page.

--- haiku.js
+++ haiku.js
@@ -66,6 +66,11 @@ if (Meteor.isClient) {
       }
     });
 
+    Template.defaultLayout.helpers({
+      'routeName' : function () {
+        return FlowRouter.getRouteName();
+      }
+    });
 
     Template.homeLayout.helpers({
         'randLinesFive1' : function () {
